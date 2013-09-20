73a52ef61763c46d9db13a295ed9ff3d1f35ce7b
Tony Schick <schick.anthony@gmail.com>
2013-09-20 11:34:52 -0500

    form fixes and training template clean

--- tracker/list/forms.py
+++ tracker/list/forms.py
@@ -2,19 +2,11 @@ from tracker.list.models import Article, Impact, Training, TrainingType
 from django import forms
 
 class ContactForm(forms.Form):
-	eventtype = forms.ModelChoiceField(queryset=TrainingType.objects.all().order_by('name'))
+	event_type = forms.ModelChoiceField(queryset=TrainingType.objects.all().order_by('name'), required=False)
 	email = forms.EmailField()
 	byline = forms.CharField(required=False)
 	headline = forms.CharField()
 	hyperlink = forms.URLField(initial='http://')
-	pub_date = forms.DateField(initial='mm/dd/yyyy',
-						   widget=forms.TextInput(attrs=
-                                {
-                                    'class':'datepicker'
-                                }))
-	event_date = forms.DateField(initial='mm/dd/yyyy',
-						   widget=forms.TextInput(attrs=
-                                {
-                                    'class':'datepicker'
-                                }))
+	pub_date = forms.DateField(initial='mm/dd/yyyy')
+	event_date = forms.DateField(initial='mm/dd/yyyy', required=False)
 	story_summary = forms.CharField(widget=forms.Textarea(attrs={'style': 'width:500px'}), required=False, )
